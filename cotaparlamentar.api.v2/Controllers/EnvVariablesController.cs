6edfb3b560c49dbdc3388dbc2cb09208d42f2213
wsc0035 <86922054+wsc0035@users.noreply.github.com>
2022-03-27 14:11:59 -0300

    vars ambiente

---
+++ cotaparlamentar.api.v2/Controllers/EnvVariablesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cotaparlamentar.api.v2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnvVariablesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Varaveis(string var)
+        {
+            try
+            {
+                var ApiUrl = Environment.GetEnvironmentVariable("ApiUrl");
+                var Legislatura = Environment.GetEnvironmentVariable("Legislatura");
+                return Ok($"ApiUrl = {ApiUrl}, Legislatura = {Legislatura}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
